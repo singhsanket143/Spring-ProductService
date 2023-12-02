35e239bff755b3a6a58829280c102634caf3f79f
Sanket Singh <singhsanket143@gmail.com>
2023-12-02 21:48:44 +0530

    added fakestore dto
    https://github.com/singhsanket143/SpringNotes/blob/main/4.WhyDTOisImp.md

---
+++ src/main/java/com/example/productservice/models/Category.java
@@ -0,0 +1,6 @@
+package com.example.productservice.models;
+
+public class Category {
+
+    private String name;
+}
--- src/main/java/com/example/productservice/viewModels/FakeStoreProductDto.java
+++ src/main/java/com/example/productservice/viewModels/FakeStoreProductDto.java
@@ -1,10 +1,10 @@
-package com.example.productservice.models;
+package com.example.productservice.viewModels;
 
-public class Product {
+public class FakeStoreProductDto {
     private Long id;
     private String title;
     private String description;
     private String image;
     private double price;
-    private Category category;
+    private String category;
 }
