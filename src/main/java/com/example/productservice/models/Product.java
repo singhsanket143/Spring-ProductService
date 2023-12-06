dcdc0fb95a3fc1efb0205f9879002e0c74b95529
Sanket Singh <singhsanket143@gmail.com>
2023-12-07 02:16:03 +0530

    Created category and product tables using JPA
    https://github.com/singhsanket143/SpringNotes/blob/main/9.%20ManyToOne%20and%20Entity%20Annotation
    https://github.com/singhsanket143/SpringNotes/blob/main/8.SpringJPA.md
    https://stackoverflow.com/questions/42135114/how-does-spring-jpa-hibernate-ddl-auto-property-exactly-work-in-spring

--- src/main/java/com/example/productservice/models/Product.java
+++ src/main/java/com/example/productservice/models/Product.java
@@ -1,15 +1,23 @@
 package com.example.productservice.models;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.UUID;
+
 @Getter
 @Setter
+@Entity
 public class Product {
-    private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
     private String title;
     private String description;
     private String image;
     private double price;
+    @ManyToOne
     private Category category;
+
 }
